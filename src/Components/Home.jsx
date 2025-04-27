c91751164e43d1c90a1e877a6015db7def09b6fb
Saurav Kumar <165908739+saurav2520@users.noreply.github.com>
2025-04-27 19:32:02 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className='md:w-2/4 md:pt-10 m-1'>
         <h1 className='text-3xl md:text-6xl font-bold flex leading-normal tracking-tighter '><TextChange/></h1>
         <p className='text-sm md:text-2xl traking-tight '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos dignissimos voluptate </p>
-       <a href="https://drive.google.com/file/d/1i9Nx2AmuDf_Ox8qGO9nc1obDuAXU0xVG/view?usp=drive_link"> <button className='mt-5 md:md-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Resume</button></a>
+       <a href="https://drive.google.com/drive/folders/1Z4PUsTyffuK4nAdgMDnIq77a7ci794sw?usp=drive_link"> <button className='mt-5 md:md-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Resume</button></a>
         </div>
         <div><img src="../pic1.jpg" alt=""  className='max-h-100 mx-16 rounded-4xl bg-white '/></div>
       
